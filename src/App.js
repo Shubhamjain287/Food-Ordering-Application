72bc67cc7edcfa94f75a991f01c172ea982395b7
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2023-02-06 23:51:43 +0530

    Namaste React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+const App = () => {
+    return (
+        <>
+            <h1> Hello Swiggy </h1>
+            <Header shubham="shubham" jain="jain" />
+        </>
+    )
+} 
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = (props) => {
+    console.log(props);
+    return (
+        <h1> Header {props.shubham} {props.jain} </h1>
+    )
+}
+
+export default Header;
